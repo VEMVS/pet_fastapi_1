34e5f36c0441b11ffb3dfe40ffb5079c74765b41
Taras Oksencyk <taras.oksenchuk@ya.ru>
2025-08-13 16:49:58 +0500

    minor changes made

--- app/database.py
+++ app/database.py
@@ -8,10 +8,17 @@ DB_HOST = '172.17.128.1'
 DB_PORT = '5432'
 DB_NAME = 'fastapi'
 
-SQLACHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(SQLACHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/main.py
+++ app/main.py
@@ -1,141 +1,79 @@
 from fastapi import FastAPI, HTTPException, status, Response, Depends
-from fastapi.params import Body
-from pydantic import BaseModel, Field, conint
-from typing import Optional, List, Dict, Any, Annotated
-from random import randrange
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from . import models
-from database import engine, SessionLocal
-from sqlalchemy import inspect
+from typing import List
+from . import models, schemas
+from .database import engine, get_db
 from sqlalchemy.orm import Session
 
 
-
-inspector = inspect(engine)
-if "users" not in inspector.get_table_names():
-    print("Таблица 'users' не найдена — создаю её")
-    from models import Base
-
-    Base.metadata.create_all(bind=engine)
-else:
-    print("Таблица 'users' уже есть — пропускаю создание")
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.get("/sqlalchemy")
+# ---- GET: вернуть список постов ----
+@app.get("/posts", response_model=List[schemas.PostResponse])
 def test_posts(db: Session = Depends(get_db)):
-
-    
-class Post(BaseModel):
-    title: str = Field(..., min_length=1, max_length=200)
-    content: str = Field(..., min_length=1)
-    published: bool = True
+    return db.query(models.Post).all()
 
 
-DB_USER = "postgres"
-DB_PASSWORD = "18491256"
-DB_HOST = "172.17.128.1"
-DB_PORT = "5432"
-DB_NAME = "fastapi"
+@app.get("/posts/{post_id}", response_model=schemas.PostResponse)
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    find_post = db.query(models.Post).filter(models.Post.id == post_id).first()
 
-try:
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=DB_PORT,
-        cursor_factory=RealDictCursor,
-    )
-    cursor = conn.cursor()
-    print("Database connection was succesfull!")
-except Exception as error:
-    print("Connecting to database failed")
-    print("Error: ", error)
-
-
-@app.get("/posts/")
-def get_post():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    print(posts)
-
-    return {"data": posts}
-
-
-@app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * from posts WHERE id = %s """, (str(id)))
-    post = cursor.fetchone()
-
-    if not post:
+    if find_post is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"post with: {id} was not found",
+            status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
         )
 
-    return {"Post_deatail": post}
+    return find_post
 
 
-@app.post("/posts/")
-def create_posts(post: Post):
-    # cursor.execute(f"INSERT INTO posts (title, content, published) VALUES ({post.title})")
+# ---- POST: создать пост ----
+@app.post(
+    "/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse
+)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
 
-    cursor.execute(
-        """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) 
-                   RETURNING *""",
-        (post.title, post.content, post.published),
-    )
-
-    new_post = cursor.fetchone()
-    conn.commit()
-
-    return {"data": new_post}
+    return new_post
 
 
-@app.delete("/posts/{id}")
-def delete_post(id: int):
-    cursor.execute(
-        """DELETE FROM posts WHERE id = %s returning *""",
-        (str(id)),
-    )
-    delete_post = cursor.fetchone()
+@app.put("/posts/{post_id}", response_model=schemas.PostResponse)
+def put_post(
+    post_id: int, update_post: schemas.PostUpdate, db: Session = Depends(get_db)
+):
+    post_query = db.query(models.Post).filter(models.Post.id == post_id)
+    post = post_query.first()
 
-    if delete_post is None:
+    if post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with: {id} was not found",
+            detail=f"Post with id: {post_id} does not exist",
         )
-    conn.commit()
 
-    return {"delete_post": delete_post}
+    post_query.update(update_post.model_dump(), synchronize_session=False)
+    db.commit()
+
+    updatet_post = post_query.first()
+
+    return updatet_post
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title = %s, content = %s, published = %s 
-                   WHERE id = %s returning *""",
-        (post.title, post.content, post.published, str(id)),
-    )
-    update_post = cursor.fetchone()
-    conn.commit()
+@app.delete("/posts/{post_id}")
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    post_delete = db.query(models.Post).filter(models.Post.id == post_id).first()
 
-    if update_post is None:
+    if post_delete is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Post with: {id} does not exist",
+            detail=f"Post with id: {post_id} does not exist",
         )
 
-    return {"Post_update": update_post}
+    db.delete(post_delete)
+    db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
